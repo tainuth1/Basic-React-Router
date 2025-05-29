bc571a42e6247f3bdd956087bb9b5ac547a3910b
tainuth1 <tainuth1@gmail.com>
2025-05-29 11:50:25 +0700

    design form

--- src/pages/dashboard/Main.jsx
+++ src/pages/dashboard/Main.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import "./main.css";
 
 const Main = () => {
   return (
-    <div>
-      <h1>This is main page.</h1>
+    <div className="parent-form">
+      <form>
+        <h2 style={{textAlign: "center"}}>Add Product</h2>
+        <input type="text" placeholder="Enter Name" />
+        <input type="number" placeholder="Enter Price" />
+        <select>
+          <option value="Food">Food</option>
+          <option value="Technology">Technology</option>
+          <option value="Drink">Drink</option>
+          <option value="Phone">Phone</option>
+          <option value="Computer">Computer</option>
+        </select>
+        <input type="url" placeholder="Enter image url" />
+        <button>Add Product</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
